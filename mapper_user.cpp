6f360286e54c06228f3c89f6bbd16a5867118951
bluesky <379865549@qq.com>
2021-06-08 00:48:42 +0800

    对用户进行增删改查接口的封装

---
+++ mapper_user.cpp
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "mapper_user.h"
+
+// 添加用户 
+// 删除用户 
+// 修改用户 
+//////////////////////////////////查找用户/////////////////////////////////////////////// 
+
+// 通过用户名查找 
+int select_user_by_username(USERS users,char *p_username)
+{
+	int i;
+	for (int = 0; i < users.length; i++)
+	{
+		if (strcmp(users.users[i].user_name ,p_username) == 0)	// 用户名一致
+		{
+			return i;		// 返回索引位置 
+		} 
+	} 
+	return -1;
+}
---
+++ mapper_user.h
@@ -0,0 +1,11 @@
+#ifndef __MAPPER_USER_H__
+#define __MAPPER_USER_H__ 
+
+// 添加用户 
+// 删除用户 
+// 修改用户 
+//////////////////////////////////查找用户/////////////////////////////////////////////// 
+
+// 通过用户名查找 
+int select_user_by_username(USERS users,char *p_username);
+#endif
