854122d78b66ba98ffeffe376bd4a67f130332ce
bluesky <379865549@qq.com>
2021-06-08 01:29:23 +0800

    添加用户姓名

--- struct.h
+++ struct.h
@@ -1,22 +1,24 @@
 #ifndef __STRUCT_H__
 #define __STRUCT_H__
 
-#define USER_NAME_LEN           30		// 用户名长度 
+#define USERNAME_LEN            30		// 用户名长度 
 #define USER_NUM_MAX			1000	// 用户最大数量 
+#define NAME_LEN				30      // 用户姓名		
 #define PWD_LEN                 30  	// 密码长度 
 #define EDUCATION_LEN			30  	// 学历存放数组长度 
 #define DEPART_NAME_LEN         30 		// 部门名称长度 
 #define DEPART_NUM_MAX			50		// 部门最大数量 
 #define DIR_LEN_MAX				256 	// 路径的最大长度 
 struct USER{
-	char user_name[USER_NAME_LEN];	// 用户名 
+	char user_name[USERNAME_LEN];	// 用户名 
+	char name[NAME_LEN];			// 姓名 
 	char password[PWD_LEN];			// 密码 
 	int role_id;					// 角色ID：0 一般员工, 1 部门管理员, 2 系统管理员 
 	int sex; 						// 性别 
 	int department_id;				// 部门id 
 	char education[EDUCATION_LEN];	// 学历 
 	int employee_id;			    // 员工号 
-	bool status;						// 在职状态 1 入职，0离职 
+	bool status;					// 在职状态 1 入职，0离职 
 };
 
 struct DEPARTMENT{
