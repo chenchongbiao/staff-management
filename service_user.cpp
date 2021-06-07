e4634757257d8ba124f99540aab4ed44793bb95f
bluesky <379865549@qq.com>
2021-06-08 00:49:31 +0800

    用户相关业务的接口

---
+++ service_user.cpp
@@ -0,0 +1,15 @@
+#include "service_user.h"
+#include "struct.h"
+bool login(USERS users, USER *p_user) // 用户登录 
+{
+	USER user; // 存放用户实体 
+	int index; // 用户索引位置 
+	while(true)
+	{
+		printf("username:");
+		scanf("%s", p_user->user_name);
+		printf("password: ");
+		scanf("%s", p_user->password);
+		in 
+	} 
+}
---
+++ service_user.h
@@ -0,0 +1,4 @@
+#ifndef __SERVICE_USER_H__
+#define __SERVICE_USER_H__
+bool login(USERS users, USER *p_user); // µÇÂ¼ 
+#endif 
