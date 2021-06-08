7612e2545f4d8dfe4d9f23eb9951c8386ca6f450
bluesky <379865549@qq.com>
2021-06-08 17:28:57 +0800

    添加修改字体颜色工具函数

--- utils.cpp
+++ utils.cpp
@@ -1,1 +1,9 @@
 #include "utils.h"
+
+//改变字体颜色 
+void color(short x) {  
+    if(x>=0 && x<=15)//参数在0-15的范围颜色  
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x); 
+    else
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);  //默认的颜色白色  
+} 
--- utils.h
+++ utils.h
@@ -2,4 +2,7 @@
 #define __UTILS_H__
 #include <stdio.h>
 #include <windows.h> 
+#include "utils.h"
+//改变字体颜色 
+void color(short x);
 #endif 
