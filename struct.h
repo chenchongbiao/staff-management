06aa0798b2e9e87413f52f40664e2c3b44da7772
bluesky <379865549@qq.com>
2021-06-08 12:01:31 +0800

    修改学历的表示类型

--- struct.h
+++ struct.h
@@ -5,7 +5,6 @@
 #define USER_NUM_MAX			1000	// 用户最大数量 
 #define NAME_LEN				30      // 用户姓名		
 #define PWD_LEN                 30  	// 密码长度 
-#define EDUCATION_LEN			30  	// 学历存放数组长度 
 #define DEPART_NAME_LEN         30 		// 部门名称长度 
 #define DEPART_NUM_MAX			50		// 部门最大数量 
 #define DIR_LEN_MAX				256 	// 路径的最大长度 
@@ -14,9 +13,9 @@ struct USER{
 	char name[NAME_LEN];			// 姓名 
 	char password[PWD_LEN];			// 密码 
 	int role_id;					// 角色ID：0 一般员工, 1 部门管理员, 2 系统管理员 
-	int sex; 						// 性别 
+	int sex; 						// 性别  0 表示女，1 表示男 
 	int department_id;				// 部门id 
-	char education[EDUCATION_LEN];	// 学历 
+	int education;					// 学历    0小学 1初级中学 2高级中学 3中专 4职校 5中技 6专科 7本科 8硕士研究生 9博士研究生  
 	int employee_id;			    // 员工号 
 	bool status;					// 在职状态 1 入职，0离职 
 };
